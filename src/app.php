ad4a6e433c41dcf031343dd68d4238b6f2f2b006
Grant Tepper <grant.tepper@gmail.com>
2017-01-07 02:13:03 -0800

    adds redis

--- src/app.php
+++ src/app.php
@@ -11,10 +11,6 @@ $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
-$app['twig'] = $app->extend('twig', function ($twig, $app) {
-    // add custom globals, filters, tags, ...
 
-    return $twig;
-});
 
 return $app;
