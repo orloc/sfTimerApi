39695065cf6f114ee1b45b5b1ec4d83078fdc9d2
Grant Tepper <grant.tepper@gmail.com>
2017-07-21 01:47:38 -0700

    updating w ith tests

---
+++ tests/UserControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Silex\WebTestCase;
+
+if (!class_exists('\PHPUnit\Framework\TestCase', true)) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+} elseif (!class_exists('\PHPUnit_Framework_TestCase', true)) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
+
+class UserControllerTest extends WebTestCase
+{
+    public function testBaseRoute()
+    {
+        $client = $this->createClient();
+        $client->followRedirects(true);
+        
+        $client->request('GET', '/');
+        
+        
+        $resp = $client->getResponse();
+        $this->assertTrue($resp->isNotFound());
+    }
+
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../app/app.php';
+        $app['session.test'] = true;
+
+        return $this->app = $app;
+    }
+}
